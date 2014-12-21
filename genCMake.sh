caafdd1edb07d7555b1b383524686715b13b2b42
vinayyadav3016 <vinayyadav3016@gmail.com>
2014-12-22 00:55:37 +0530

    added cmake generetor

---
+++ genCMake.sh
@@ -0,0 +1,47 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: genCMake.sh
+# 
+#         USAGE: ./genCMake.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: Sunday 21 December 2014 20:30
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+mkdir -p build
+cd build
+str=""
+case $1 in
+  avr)
+    str="-DCMAKE_TOOLCHAIN_FILE=~/cmakefiles/avr-toolchain.cmake"
+    cmake "${str}" $@ ..
+    make
+    shift
+    ;;
+  arm)
+    str="-DCMAKE_TOOLCHAIN_FILE=~/cmakefiles/arm-toolchain.cmake"
+    source ${PROJECTS}/ros-projects-arm/env-arm-ros.sh
+    cmake "${str}" $@ ..
+    make
+    shift
+    ;;
+  pc)
+    cmake $@ ..
+    make
+    ;;
+  *)
+    echo "Error please give a platform to build tool"
+    ;;
+esac
+cd ..
