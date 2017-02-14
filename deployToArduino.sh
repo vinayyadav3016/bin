a2db46ee7d8c3a6f2c8603e23fac080965091026
vinayyadav3016 <vinayyadav3016@gmail.com>
2017-02-14 23:23:55 +0530

    -- removed default values to make sure nothing breaks in Arduino

--- deployToArduino.sh
+++ deployToArduino.sh
@@ -19,11 +19,21 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+error() {
+	echo "Usage    : -f flash -p port -d device -b buadrate -u uploader -h for help"
+	echo "Flash    : main.hex"
+	echo "Device   : atmega328p"
+	echo "Port     : /dev/ttyACM0"
+	echo "Baudrate : 115200"
+	echo "Uploader : arduino(for 328p), wiring(mega2560)"
+	exit $@
+}
+
 HEX_FILE=""
-DEVICE=atmega328p
-PORT=/dev/ttyACM0
-BAUDRATE=115200
-UPLOADER=arduino
+DEVICE=""
+PORT=""
+BAUDRATE=""
+UPLOADER=""
 TEMP=`getopt -o f:d:b:p:u:h --long flash:,device:,baudrate:,port:,uploader:  -- "$@"`
 eval set -- "$TEMP"
 while true
@@ -53,12 +63,38 @@ do
       shift;
       break;;
     -h)
-      echo " usage : -f flash -p port -d device -b buadrate -u uploader -h for help"
-      exit 0;;
+      error 0;;
     *)
-     exit 0
+     error 0
   esac
 done
+
+if [[ -z "$HEX_FILE" ]]
+then
+	echo "Hex"
+	error 1
+fi
+
+if [[ -z "$DEVICE" ]]
+then
+	echo "device"
+	error 1
+fi
+
+if [[ -z "$PORT" ]]
+then
+	error 1
+fi
+
+if [[ -z "$BAUDRATE" ]]
+then
+	error 1
+fi
+
+if [[ -z "$UPLOADER" ]]
+then
+	error 1
+fi
 
 if [[ -c "$PORT" ]]
 then
@@ -71,7 +107,8 @@ then
     echo "Baudrate : $BAUDRATE"
     echo "Uploader : $UPLOADER"
     #avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf\
-    avrdude -C/usr/share/arduino/hardware/tools/avr/etc/avrdude.conf\
+    #avrdude -C/usr/share/arduino/hardware/tools/avr/etc/avrdude.conf\
+    avrdude -C/etc/avrdude.conf -v \
       -p$DEVICE -c$UPLOADER -P$PORT -b$BAUDRATE -D -Uflash:w:$HEX_FILE:i
   else
     echo "File $HEX_FILE do not exist"
